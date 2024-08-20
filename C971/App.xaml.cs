8632d68c2b0309414ab7040592be6c0b42bf8ecb
Nanouq <nanouq@hotmail.com>
2024-08-20 14:32:11 -0400

    Initial commit

---
+++ C971/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace C971
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
